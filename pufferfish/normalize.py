e75385067bb0f6f1d11397e190907757b1cff471
JohnUrban <mr.john.urban@gmail.com>
2020-09-25 10:40:16 -0400

    New protocols added. Finalization steps of logging and smoothing added.

--- pufferfish/normalize.py
+++ pufferfish/normalize.py
@@ -8,6 +8,7 @@ class NormalizeProtocol(object):
         #latestage, protocol=1, earlystage=False, pseudo=0.1, bandwidth=2500, quiet=False, impute=False, replace=False, replace_with='0', replace_this='.'
         self.args = args
         self._normalize()
+        self._finalize()
         
     def _set_protocol(self):
         if self.args.protocol1:
@@ -43,61 +44,96 @@ class NormalizeProtocol(object):
         elif self.args.protocol11:
             self.protocol = 11
             self._run_protocol = self._protocol11
-
+        elif self.args.protocol12:
+            self.protocol = 12
+            self._run_protocol = self._protocol12
+        elif self.args.protocol13:
+            self.protocol = 13
+            self._run_protocol = self._protocol13
+        elif self.args.protocol14:
+            self.protocol = 14
+            self._run_protocol = self._protocol14
+        elif self.args.protocol15:
+            self.protocol = 15
+            self._run_protocol = self._protocol15
+        elif self.args.protocol16:
+            self.protocol = 16
+            self._run_protocol = self._protocol16
+        elif self.args.protocol17:
+            self.protocol = 17
+            self._run_protocol = self._protocol17
+            
     def _protocol1(self):
         self.late.median_normalize_data()
         if self.early:
             self.early.median_normalize_data()
             self.late.normalize_to_other(self.early,
-                                              self.args.pseudocount)
+                                              self.args.pseudo)
 
     def _protocol2(self):
-        print self.late.get_median()
-        self.late.ksmooth_counts(bw=self.args.bandwidth)
+        #print self.late.get_median()
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
         self.late.median_normalize_data()
         if self.early:
-            self.early.ksmooth_counts(bw=self.args.bandwidth)
+            self.early.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
             self.early.median_normalize_data()
             self.late.normalize_to_other(self.early,
-                                              self.args.pseudocount)
+                                              self.args.pseudo)
 
     def _protocol3(self):
         self.late.median_normalize_data()
-        self.late.ksmooth_counts(bw=self.args.bandwidth)
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
         if self.early:
             self.early.median_normalize_data()
-            self.early.ksmooth_counts(bw=self.args.bw)
+            self.early.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
             self.late.normalize_to_other(self.early,
-                                         self.args.pseudocount)
+                                         self.args.pseudo)
 
     def _protocol4(self):
         self.late.median_normalize_data()
         if self.early:
             self.early.median_normalize_data()
             self.late.normalize_to_other(self.early,
-                                         self.args.pseudocount)
-        self.late.ksmooth_counts(bw=self.args.bandwidth)
+                                         self.args.pseudo)
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
 
     def _protocol5(self):
         #smoothing only --  late/early before smooth
         if self.early:
             self.late.normalize_to_other(self.early,
-                                         self.args.pseudocount)
-        self.late.ksmooth_counts(bw=self.args.bandwidth)
+                                         self.args.pseudo)
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
 
     def _protocol6(self):
         #smoothing only --  late/early AFTER smooth
-        self.late.ksmooth_counts(bw=self.args.bandwidth)
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
         if self.early:
-            self.early.ksmooth_counts(bw=self.args.bandwidth)
+            self.early.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
             self.late.normalize_to_other(self.early,
-                                         self.args.pseudocount)
+                                         self.args.pseudo)
 
     def _protocol7(self):
         #no smoothing, no median norm
         # late:early only if early present
         if self.early:
-            self.late.normalize_to_other(self.early, pseudocount)
+            self.late.normalize_to_other(self.early,
+                                         self.args.pseudo)
 
     def _protocol8(self):
         self.late.computeSkew()
@@ -110,6 +146,82 @@ class NormalizeProtocol(object):
 
     def _protocol11(self):
         self.late.computePercentChange()
+
+    def _protocol12(self):
+        # median ratio norm - e.g. similar to DEseq (or TMM in EdgeR) -- global version
+        # if no "early" sample present, then this just returns median norm late -- like protocol 1
+        if self.early:
+            self.late.normalize_to_other(self.early,
+                                         self.args.pseudo)
+        self.late.median_normalize_data()
+
+    def _protocol13(self):
+        # median ratio norm w/ pre-smoothing - e.g. similar to DEseq (or TMM in EdgeR) -- global version
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
+        if self.early:
+            self.early.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
+            self.late.normalize_to_other(other=self.early,
+                                         pseudocount=self.args.pseudo)
+        self.late.median_normalize_data()
+
+    def _protocol14(self):
+        # median ratio norm w/ post-smoothing - e.g. similar to DEseq (or TMM in EdgeR) -- global version
+        if self.early:
+            self.late.normalize_to_other(self.early,
+                                         self.args.pseudo)
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
+        self.late.median_normalize_data()
+        
+
+    def _protocol15(self):
+        # median ratio norm w/ end-smoothing - e.g. similar to DEseq (or TMM in EdgeR) -- global version
+        if self.early:
+            self.late.normalize_to_other(self.early,
+                                         self.args.pseudo)
+        self.late.median_normalize_data()
+        self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
+
+    def _protocol16(self):
+        # experimental glocal median ratio norm w/ end-smoothing - 
+        self.late.normalize_with_glocalMedRatioNorm(other=self.early,
+                                                    pseudocount=self.args.pseudo,
+                                                    globalweight=100,
+                                                    minlocalbins=5,
+                                                    minpropdata=0.333) ## LAST 3 HARDCODED FOR NOW -- less local
+    def _protocol17(self):
+        ## First median normalizes both samples
+        ## Then scales to a target median bin cov -- default 1000.
+        ## Then adds pseudocount - default 1. (so default pseudo is 0.1% median, 1000-fold less than median)
+        ## Then normalizes late to early
+        ## Then median ratio normalizes a la DEseq/EdgeR
+        self.late.median_normalize_data()
+        self.late.scale_data(scale=self.args.scalecov)
+        if self.early:
+            self.early.median_normalize_data()
+            self.early.scale_data(scale=self.args.scalecov)
+            
+            self.late.normalize_to_other(self.early,
+                                              self.args.pseudo)
+        self.late.median_normalize_data(relearn=True)
+
+
+    def _protocol18(self):
+        ## Similar to 17, but only adds pseudo to bins with 0....
+        ##
+        ## First median normalizes both samples
+        ## Then scales to a target median bin cov -- default 1000.
+        ## Then adds pseudocount - default 1. (so default pseudo is 0.1% median, 1000-fold less than median)
+        ## Then normalizes late to early
+        ## Then median ratio normalizes a la DEseq/EdgeR
+        pass
 
         
     def _normalize(self):
@@ -151,6 +263,19 @@ class NormalizeProtocol(object):
         self._set_protocol()
         newmsg("following normalization protocol "+str(self.protocol)+emsg)
         self._run_protocol()
+
+    def _finalize(self):
+        ## Optional Log Transforms
+        if self.args.log10:
+            self.late.log10_data()
+        elif self.args.log2:
+            self.late.log2_data()
+
+        ## Optional end smoothing
+        if self.args.endsmoothing:
+            self.late.ksmooth_counts(bw=self.args.bandwidth,
+                                      rescueNaN=self.args.replaceNaN,
+                                      localWindow=self.args.localwinsize)
 
 
 ##TODO - allow imputing values locally when a bin is 0 -- i.e. if surrounded by 2 bins with values >0, take average.
